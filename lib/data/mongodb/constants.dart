f99cd8e72c65143b184a82faeed2cc1a8163b974
Shubham-Zone <154971588+Shubham-Zone@users.noreply.github.com>
2024-10-21 22:29:41 +0530

    Update constants.dart

--- lib/data/mongodb/constants.dart
+++ lib/data/mongodb/constants.dart
@@ -1,3 +1,6 @@
-var username = "YOUR_USER_NAME";
-var pass = "YOUR_PASS";
-var url = "KEY";
+var username = "devshubham652";
+var pass = "FMKzEX8dpWIBCEiv";
+var url = "mongodb+srv://devshubham652:QFgkKBwqUZubn3sZ@cluster0.fkwjh8k.mongodb.net/RTO?retryWrites=true&w=majority&appName=Cluster0";
+// var url = "mongodb+srv://devshubham652:FMKzEX8dpWIBCEiv@cluster0.fkwjh8k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// var URL = "mongodb+srv://devshubham652:<password>@cluster0.fkwjh8k.mongodb.net/"
+// mongodb+srv://devshubham652:Shubham@90500@cluster0.fkwjh8k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
